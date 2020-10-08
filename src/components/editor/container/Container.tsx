811b9fc8a03927572d956e66d285034fa0f6f1d4
Peter Knight <petekanighit@gmail.com>
2020-10-08 19:18:10 +1300

    Basic zooming

--- src/components/editor/container/Container.tsx
+++ src/components/editor/container/Container.tsx
@@ -1,4 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
+
+interface ContainerProps {
+  maxZoom?: number;
+}
 
 interface Point {
   x: number;
@@ -10,68 +14,119 @@ interface ViewBox extends Point {
   height: number;
 }
 
-function getPointFromEvent(event: React.MouseEvent): Point {
-  return { x: event.clientX, y: event.clientY };
-}
-
 function getViewBoxString(viewBox: ViewBox): string {
   const { x, y, width, height } = viewBox;
   return `${x} ${y} ${width} ${height}`;
 }
 
-function Container() {
-  const [viewBox, setViewBox] = useState<ViewBox>({
+function Container({ maxZoom = 10 }: ContainerProps) {
+  const originalViewBox = {
     x: 0,
     y: 0,
-    width: 300,
-    height: 300
-  });
-  const [pointerDown, setPointerDown] = useState(false);
+    width: 1024,
+    height: 1024
+  };
+  const [viewBox, setViewBox] = useState<ViewBox>(originalViewBox);
+  const [viewBoxRatio, setViewBoxRatio] = useState(1);
+  const [panning, setPanning] = useState(false);
   const [pointer, setPointer] = useState<Point>({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
   const svgRef = useRef<SVGSVGElement>(null);
 
+  const viewBoxString = useMemo(() => {
+    return getViewBoxString(viewBox);
+  }, [viewBox]);
+
+  useEffect(() => {
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  });
+
+  useEffect(() => {
+    if (svgRef.current) {
+      setViewBoxRatio(
+        viewBox.width / svgRef.current.getBoundingClientRect().width
+      );
+    }
+  }, [viewBox.width, setViewBoxRatio]);
+
+  function getClientPointFromEvent(event: React.MouseEvent): Point {
+    return { x: event.clientX, y: event.clientY };
+  }
+
+  function getLocalPointFromClientPoint(point: Point): Point {
+    if (!svgRef.current) return point;
+    const clientPoint = svgRef.current.createSVGPoint();
+    clientPoint.x = point.x;
+    clientPoint.y = point.y;
+    return clientPoint.matrixTransform(
+      svgRef.current.getScreenCTM()?.inverse()
+    );
+  }
+
   function handleMouseDown(event: React.MouseEvent): void {
-    setPointer(getPointFromEvent(event));
-    setPointerDown(true);
+    if (event.button === 0) {
+      setPanning(true);
+      setPointer(getClientPointFromEvent(event));
+    }
   }
 
   function handleMouseUp(): void {
-    setPointerDown(false);
-  }
-
-  function handleMouseLeave(): void {
-    setPointerDown(false);
+    setPanning(false);
   }
 
   function handleMouseMove(event: React.MouseEvent): void {
-    if (pointerDown && svgRef.current) {
-      const ratio =
-        viewBox.width / svgRef.current.getBoundingClientRect().width;
-      const newPointer = getPointFromEvent(event);
+    if (panning) {
+      const newPointer = getClientPointFromEvent(event);
       setViewBox({
-        x: viewBox.x - (newPointer.x - pointer.x) * ratio,
-        y: viewBox.y - (newPointer.y - pointer.y) * ratio,
+        x: viewBox.x - (newPointer.x - pointer.x) * viewBoxRatio,
+        y: viewBox.y - (newPointer.y - pointer.y) * viewBoxRatio,
         width: viewBox.width,
         height: viewBox.height
       });
-      setPointer(getPointFromEvent(event));
+      setPointer(newPointer);
     }
   }
 
   function handleWheel(event: React.WheelEvent) {
-    console.log(event.deltaY);
+    const clientCursor = getClientPointFromEvent(event);
+    const localCursor = getLocalPointFromClientPoint(clientCursor);
+
+    let newZoom: number;
+    if (event.deltaY < 0) {
+      newZoom = zoom + 1;
+    } else {
+      newZoom = zoom - 1;
+    }
+    if (newZoom < 1 || newZoom > maxZoom) return;
+
+    const width = originalViewBox.width / Math.pow(2, newZoom - 1);
+    const height = originalViewBox.height / Math.pow(2, newZoom - 1);
+    const x =
+      viewBox.x -
+      (localCursor.x / originalViewBox.width) * (width - viewBox.width);
+    const y =
+      viewBox.y -
+      (localCursor.y / originalViewBox.height) * (height - viewBox.height);
+    setViewBox({
+      x,
+      y,
+      width,
+      height
+    });
+    setZoom(newZoom);
   }
 
   return (
     <svg
       ref={svgRef}
-      viewBox={getViewBoxString(viewBox)}
+      viewBox={viewBoxString}
       onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseLeave}
       onMouseMove={handleMouseMove}
       onWheel={handleWheel}>
-      <rect x="150" y="100" width="100" height="100" />
+      <rect x="462" y="462" width="100" height="100" />
     </svg>
   );
 }
