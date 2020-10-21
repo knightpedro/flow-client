d0a4eae28cda85ae4d9d6261064fcdb5c4d20e28
Peter Knight <petekanighit@gmail.com>
2020-10-21 19:09:13 +1300

    Added tests

--- src/hooks/useSvgPanZoom.spec.js
+++ src/hooks/useSvgPanZoom.spec.js
@@ -1,38 +1,117 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 import { useSvgPanZoom } from './useSvgPanZoom';
 
-const PADDING = 5;
 const BBOX = {
   x: 0,
   y: 0,
   width: 100,
   height: 100
 };
+const POINT = {
+  x: 20,
+  y: 30
+};
 
 describe('useSvgPanZoom', () => {
-  let svgRef = {
-    current: {
-      getBBox: jest.fn().mockImplementation(() => BBOX)
-    }
-  };
+  let svgRef;
 
-  afterEach(() => {
-    jest.clearAllMocks();
+  beforeEach(() => {
+    svgRef = {
+      current: {
+        createSVGPoint: () => ({
+          x: 0,
+          y: 0,
+          matrixTransform: () => POINT
+        }),
+        getBBox: () => BBOX,
+        getScreenCTM: () => null
+      }
+    };
   });
 
-  it('should use svgPanZoom', () => {
+  it('calculates initial viewBox', () => {
     const { result } = renderHook(() => useSvgPanZoom(svgRef));
-    expect(result.current).not.toBeNull();
-  });
-
-  it('calculates initial viewBox with padding', () => {
-    const { result } = renderHook(() => useSvgPanZoom(svgRef));
-    const { viewBox } = result.current;
-    expect(viewBox).toEqual({
-      x: -PADDING / 2,
-      y: -PADDING / 2,
-      width: BBOX.width + PADDING,
-      height: BBOX.height + PADDING
+    expect(result.current.viewBox).toEqual({
+      x: -2.5,
+      y: -2.5,
+      width: 105,
+      height: 105
     });
+  });
+
+  it('pans to a point', () => {
+    const { result } = renderHook(() => useSvgPanZoom(svgRef));
+
+    act(() => {
+      result.current.panTo(POINT);
+    });
+
+    expect(result.current.viewBox).toEqual({
+      x: -32.5,
+      y: -22.5,
+      width: 105,
+      height: 105
+    });
+  });
+
+  it('sets panning to true on mousedown', () => {
+    const { result } = renderHook(() => useSvgPanZoom(svgRef));
+    expect(result.current.panning).not.toBeTruthy();
+    const event = new MouseEvent('mousedown');
+
+    act(() => {
+      result.current.handleMouseDown(event);
+    });
+
+    expect(result.current.panning).toBeTruthy();
+  });
+
+  it('sets panning to false on mouseup', () => {
+    const { result } = renderHook(() => useSvgPanZoom(svgRef));
+    const mouseDownEvent = new MouseEvent('mousedown');
+    const mouseUpEvent = new MouseEvent('mouseup');
+
+    expect(result.current.panning).not.toBeTruthy();
+
+    act(() => {
+      result.current.handleMouseDown(mouseDownEvent);
+    });
+
+    expect(result.current.panning).toBeTruthy();
+
+    act(() => {
+      result.current.handleMouseUp(mouseUpEvent);
+    });
+
+    expect(result.current.panning).not.toBeTruthy();
+  });
+
+  it('resets the viewBox', () => {
+    const { result } = renderHook(() => useSvgPanZoom(svgRef));
+    const initialViewBox = {
+      x: -2.5,
+      y: -2.5,
+      width: 105,
+      height: 105
+    };
+
+    // Calculate initial viewBox correctly
+    expect(result.current.viewBox).toEqual(initialViewBox);
+
+    // Pan away
+    act(() => {
+      result.current.panTo(POINT);
+    });
+
+    // ViewBox changes based on pan
+    expect(result.current.viewBox).not.toEqual(initialViewBox);
+
+    // Reset viewBox
+    act(() => {
+      result.current.reset();
+    });
+
+    // viewBox is reset to initial value
+    expect(result.current.viewBox).toEqual(initialViewBox);
   });
 });
